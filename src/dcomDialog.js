ddf723ca3681d5bb889ae79226f31c5d291bc98d
doodeec <doodeec@gmail.com>
2013-11-12 23:33:52 +0100

    refactoring

--- src/dcomDialog.js
+++ src/dcomDialog.js
@@ -3,43 +3,47 @@
 angular.module('dcomDialog', [])
     .service('dialogService',
         ['$q','$http','$templateCache','$document', '$timeout',
-            function dialogService($q, $http, $templateCache, $document, $timeout) {
+            function dialogProvider($q, $http, $templateCache, $document, $timeout) {
 
-                var dialogCount = 0;
+                var count = 0;
                 var defaultTemplate = 'templates/default.html';
                 var allDialogs = [];
                 var openedDialogs = [];
 
-                /**
-                 * Dialog constructor, returns newly created Dialog object
-                 * @param template {mandatory}
-                 * @param className
-                 * @param ctrl
-                 * @param persistent
-                 * @constructor
-                 */
+                function getDialog(prop, value) {
+                    for (var i=0, iMax=allDialogs.length; i < iMax; i++) {
+                        if (allDialogs[i][prop] == value) return allDialogs[i];
+                    }
+                    return null;
+                }
+
+                function destroyDialog() {
+                    var that = this;
+                    $timeout(function(){
+                        that.callStackArray('destroy');
+                        allDialogs.splice(allDialogs.indexOf(that),1);
+                    });
+                }
+
                 var Dialog = function(template, className, ctrl, persistent) {
                     if (!template) return;
 
-                    dialogCount++;
+                    count++;
                     var dialog = this;
 
-                    this.id = dialogCount;
+                    this.id = count;
                     this.name = template;
                     this.template = null;
                     this.className = className;
                     this.controller = null;
                     this._persistent = persistent || false;
                     this._ready = false;
-                    this._linkDefer = $q.defer();
+                    this._loadDirective = $q.defer();
                     this._originalTemplate = 'templates/' +template+ '.html';
 
                     if (angular.isObject(ctrl)) {
                         var ctrlFn = function($scope){
-                            for (var prop in ctrl){
-                                $scope[prop] = ctrl[prop];
-                            }
-
+                            for (var prop in ctrl){ $scope[prop] = ctrl[prop] }
                             ctrlFn.$inject = ['$scope'];
                             this.$scope = $scope;
                         };
@@ -59,182 +63,102 @@ angular.module('dcomDialog', [])
 
                     this.loadTemplate().then(function loadTmpSuccess(template) {
                         dialog.template = template;
-                        dialog._linkDefer.promise.then(function() {
+                        dialog._loadDirective.promise.then(function() {
                             dialog._ready = true;
                             dialog.callStackArray('ready');
                         });
-                    }, function loadTmpError() {
-                        debug.error("Dialog template not specified");
-                    });
+                    }, function loadTmpError() { console.error("Template not specified") });
                 };
 
-                /**
-                 * Returns promise of template load
-                 * @returns {promise|*|Function}
-                 */
-                Dialog.prototype.loadTemplate = function() {
-                    this.deferLoad = $q.defer();
+                Dialog.prototype = {
+                    loadTemplate: function() {
+                        this.deferLoad = $q.defer();
 
-                    if (this._ready) {
-                        this.deferLoad.resolve(this.template);
-                    } else if (this._originalTemplate) {
+                        if (this._ready) {
+                            this.deferLoad.resolve(this.template);
+                        } else if (this._originalTemplate) {
+                            var that = this;
+
+                            $http.get(this._originalTemplate, {cache: $templateCache})
+                                .success(function() { that.deferLoad.resolve(that._originalTemplate) })
+                                .error(function() { that.deferLoad.resolve(defaultTemplate) });
+                        } else {
+                            this.deferLoad.reject();
+                        }
+                        return this.deferLoad.promise;
+                    },
+                    open: function(e) {
+                        e && e.preventDefault();
+
+                        var openDefer = $q.defer(),
+                            that = this;
+
+                        var openFunction = function() {
+                            that.show();
+                            openedDialogs.unshift(that.id);
+                            openDefer.resolve();
+                            that.callStackArray('open');
+                        };
+
+                        //if dialog is ready, open Modal, else register onReady stack
+                        if (!this._ready || openedDialogs.indexOf(this.id) !== -1) {
+                            this.on('ready', openFunction);
+                        } else {
+                            openFunction();
+                        }
+
+                        return openDefer.promise;
+                    },
+                    dismiss: function(e) {
+                        e && e.preventDefault();
+
+                        var dismissDefer = $q.defer(),
+                            index = openedDialogs.indexOf(this.id);
+                        if (!this._ready || (index === -1)) {
+                            dismissDefer.reject();
+                        } else {
+                            this.hide();
+                            openedDialogs.splice(index,1);
+                            this.callStackArray('dismiss');
+                            dismissDefer.resolve();
+                        }
+
+                        return dismissDefer.promise;
+                    },
+                    destroy: function() {
                         var that = this;
 
-                        $http.get(this._originalTemplate, {cache: $templateCache})
-                            .success(function() {
-                                that.deferLoad.resolve(that._originalTemplate);
-                            })
-                            .error(function() {
-                                that.deferLoad.resolve(defaultTemplate);
-                            });
-                    } else {
-                        this.deferLoad.reject();
-                    }
+                        this.dismiss().then(function() {
+                            destroyDialog.call(that);
+                        }, function() {
+                            destroyDialog.call(that);
+                        });
+                    },
+                    on: function(event, fn) {
+                        if (!fn || !event || !angular.isFunction(fn)) return;
 
-                    return this.deferLoad.promise;
-                };
+                        if (event in this._callStack) this._callStack[event].push(fn);
+                    },
+                    callStackArray: function(stack) {
+                        var arr = this._callStack[stack] || [];
 
-                /**
-                 * Opens dialog
-                 * If dialog is not ready, it registers callStack on 'ready' event
-                 * @param e
-                 * @returns {promise|*|Function}
-                 */
-                Dialog.prototype.open = function(e) {
-                    e && e.preventDefault();
-
-                    var openDefer = $q.defer(),
-                        that = this;
-
-                    var openFunction = function() {
-                        that.showModal();
-                        openedDialogs.unshift(that.id);
-                        openDefer.resolve();
-                        that.callStackArray('open');
-                    };
-
-                    //if dialog is ready, open Modal, else register onReady stack
-                    if (!this._ready || openedDialogs.indexOf(this.id) !== -1) {
-                        this.on('ready', openFunction);
-                    } else {
-                        openFunction();
-                    }
-
-                    return openDefer.promise;
-                };
-
-                /**
-                 * Hides dialog, but leaves the template in DOM
-                 * @param e
-                 * @returns {promise|*|Function}
-                 */
-                Dialog.prototype.dismiss = function(e) {
-                    e && e.preventDefault();
-
-                    var dismissDefer = $q.defer(),
-                        index = openedDialogs.indexOf(this.id);
-                    if (!this._ready || (index === -1)) {
-                        dismissDefer.reject();
-                    } else {
-                        this.hideModal();
-                        openedDialogs.splice(index,1);
-                        this.callStackArray('dismiss');
-                        dismissDefer.resolve();
-                    }
-
-                    return dismissDefer.promise;
-                };
-
-                /**
-                 * Destroys dialog completely, with removal from DOM
-                 */
-                Dialog.prototype.destroy = function() {
-                    var that = this;
-
-                    this.dismiss().then(function() {
-                        destroyDialog.call(that);
-                    }, function() {
-                        destroyDialog.call(that);
-                    });
-                };
-
-                /**
-                 * Registers callStack actions
-                 * @param event
-                 * @param fn
-                 */
-                Dialog.prototype.on = function(event, fn) {
-                    if (!fn || !event || !angular.isFunction(fn)) return;
-
-                    if (event in this._callStack) {
-                        this._callStack[event].push(fn);
+                        for (var j=0, jMax=arr.length; j<jMax; j++) { arr[j].call(this) }
+                    },
+                    close: function() {
+                        this._persistent ? this.dismiss() : this.destroy();
                     }
                 };
 
-                /**
-                 * Calls registered callStack actions
-                 * @param stack
-                 */
-                Dialog.prototype.callStackArray = function(stack) {
-                    var arr = this._callStack[stack] || [];
 
-                    for (var j=0, jMax=arr.length; j<jMax; j++) {
-                        arr[j].call(this);
-                    }
-                };
 
-                /**
-                 * Close the dialog, hide or remove
-                 */
-                Dialog.prototype.close = function() {
-                    this._persistent ? this.dismiss() : this.destroy();
-                };
-
-                /**
-                 * Returns dialog object
-                 * @param prop
-                 * @param value
-                 * @returns {*}
-                 */
-                function getDialog(prop, value) {
-                    for (var i=0, iMax=allDialogs.length; i < iMax; i++) {
-                        if (allDialogs[i][prop] == value) {
-                            return allDialogs[i];
-                        }
-                    }
-                    return null;
+                return {
+                    create: function(template, className, ctrl, persistent) { return new Dialog(template, className, ctrl, persistent) },
+                    get: function(name) { return getDialog('name', name) },
+                    getById: function(id) { return getDialog('id', id) },
+                    allDialogs: function() { return allDialogs },
+                    openedDialogs: function() { return openedDialogs }
                 }
-
-                function destroyDialog() {
-                    var that = this;
-                    $timeout(function(){
-                      that.callStackArray('destroy');
-                      allDialogs.splice(allDialogs.indexOf(that),1);
-                    });
-                }
-
-                //Service public methods
-                this.create = function(template, className, ctrl, persistent) {
-                    return new Dialog(template, className, ctrl, persistent);
-                };
-                this.get = function(name) {
-                    return getDialog('name', name);
-                };
-                this.getByClass = function(className) {
-                    return getDialog('className', className);
-                };
-                this.getById = function(id) {
-                    return getDialog('id', id);
-                };
-                this.allDialogs = function() {
-                    return allDialogs;
-                };
-                this.openedDialogs = function() {
-                    return openedDialogs;
-                };
-
-                return this;
-    }])
+            }])
     .directive('dcomDialogWidget',
         ['dialogService','$document',
             function(dialogService,$document) {
@@ -246,19 +170,19 @@ angular.module('dcomDialog', [])
                         scope.allDialogs = dialogService.allDialogs;
                         scope.openedDialogs = dialogService.openedDialogs;
 
-                        // register onESC key event
+                        // ESC key event closes the dialog
                         $document.keyup(function(e) {
                             var topDialog = dialogService.openedDialogs()[0];
 
                             scope.$apply(function(){
                                 e.keyCode == 27 && topDialog &&
                                     dialogService.getById(topDialog).dismiss(e) &&
-                                    e.preventDefault();
+                                e.preventDefault();
                             });
                         });
                     }
                 }
-    }])
+            }])
     .directive('dcomDialog',
         ['dialogService',
             function(dialogService) {
@@ -272,19 +196,12 @@ angular.module('dcomDialog', [])
                         };
 
                         var dialog = dialogService.getById(scope.dialog.id);
-
-                        dialog.showModal = function() {
-                            elem.modal(modalOpt);
-                        };
-
-                        dialog.hideModal = function() {
-                            elem.modal('hide');
-                        };
-
-                        dialog._linkDefer.resolve();
+                        dialog.show = function() { elem.modal(modalOpt) };
+                        dialog.hide = function() { elem.modal('hide') };
+                        dialog._loadDirective.resolve();
                     }
                 }
-    }])
+            }])
     .directive('dcomDialogBackdrop',
         ['dialogService',
             function(dialogService) {
@@ -293,16 +210,12 @@ angular.module('dcomDialog', [])
                     replace: true,
                     template: '<div class="dcomDialogBackdrop" ng-class="{shown: showBackdrop()}"></div>',
                     link: function(scope, elem, attrs) {
-                        scope.showBackdrop = function() {
-                            return scope.openedDialogs().length;
-                        };
+                        scope.showBackdrop = function() { return scope.openedDialogs().length };
 
                         //bind backdrop click event to dismiss function of the topmostDialog
                         elem.on('click', function(e) {
-                            scope.$apply(function() {
-                                dialogService.getById(scope.openedDialogs()[0]).close(e);
-                            });
+                            scope.$apply(function() { dialogService.getById(scope.openedDialogs()[0]).close(e) });
                         });
                     }
                 }
-    }]);
+            }]);
